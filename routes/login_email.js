9a30772fd5f5a39e1d016f1be1bbb2dfb18847ee
Ziheng Shao <jjjjjames.szh@gmail.com>
2019-02-15 14:32:32 -0500

    email authentication (0.1.0)

--- routes/login_email.js
+++ routes/login_email.js
@@ -22,21 +22,42 @@ router.get('/', function(req, res, next) {
 
     var sql = "SELECT * FROM userlogin WHERE email = \"";
     sql = sql + email;
-    sql = sql + "\" AND pswd = \"";
-    sql = sql + password + "\"";
+    sql = sql + "\"";
     //res.send(sql);
     console.log(sql);
 
     con.connect(function(err) {
-        //if (err) throw err;
-        console.log("Connected");
-        console.log("queueing");
-        con.query(sql, function(err, result) {
-            //if (err) throw err;
-            console.log("result acquired");
-            console.log(result);
-            res.send(200);
-        });
+        if (err) {
+            res.send({"status":404});
+            throw err;
+        }
+        else {
+            console.log("Connected");
+            console.log("queueing");
+            con.query(sql, function (err, result) {
+                //if (err) throw err;
+                console.log("result acquired");
+                console.log(result);
+                if (result[0] == null){
+                    // wrong email
+                    res.send({"status":201});
+                }
+                else {
+                    var db_password = result[0].pswd;
+                    if (db_password != passwrod) {
+                        // wrong passwrod
+                        res.send({"status": 202});
+                    } else {
+                        var uid = result[0].uid;
+                        console.log("uid = " + uid);
+                        res.send({
+                            "status": "200",
+                            "uid": uid
+                        });
+                    }
+                }
+            });
+        }
     });
 
 });
