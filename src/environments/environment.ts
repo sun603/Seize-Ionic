bdb7e5bcbeca9b2fb16939d9fb8c6807346999d1
sun603 <sun603@users.noreply.github.com>
2019-02-17 14:55:17 -0500

    env config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl:"http://ec2-3-17-151-69.us-east-2.compute.amazonaws.com:3000"
 };
 
 /*
@@ -14,3 +15,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+ 
