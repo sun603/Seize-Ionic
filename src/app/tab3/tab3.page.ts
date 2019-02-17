b285631a2a63ead0daa5a2d9c036fd9efddcb5b1
sun603 <sun603@users.noreply.github.com>
2019-02-17 14:55:01 -0500

    simple logout

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service'
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+  constructor(private auth: AuthenticationService){}
+  logOut(){
+    this.auth.logout();
+  }
+}
