b7964a06f7c34717416866ecd5da28e64fc4da90
zhan2819 <jasmine199910@gmail.com>
2019-02-19 18:50:57 -0500

    angular part of editprofile.page.ts

--- src/app/editprofile/editprofile.page.ts
+++ src/app/editprofile/editprofile.page.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditprofilePage implements OnInit {
 
+
+  private firstname:any;
+  private lastname:any;
+  private username:any;
+  private gender:any;
+  private email:any;
+  private university:any;
+  private uid:any;
+  private major:any;
+  private class:any;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  update(){
+    let data ={
+      firstname: this.firstname,
+      lastname: this.lastname,
+      username: this.username,
+      gender: this.gender,
+      email: this.email,
+      university: this.university,
+      uid: this.uid,
+      major: this.major,
+      class: this.class
+    }
   }
 
 }
