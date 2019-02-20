11be3449706ddb4df0eba76c051a1580b6ab78fb
WUu422 <wu1037@purdue.edu>
2019-02-20 22:14:17 +0000

    modified password verification

--- routes/pass_veri.js
+++ routes/pass_veri.js
@@ -1,25 +1,26 @@
 module.export = function(password){
+
+
 	var pass_veri = '';
 	for(i = 0; i < password.length; i++){
 			if(password.length < 6){
-				if (y.search[/0-9/] < 1) {
- 				pass_veri += "Your password needs a minimum of six characters.";
+ 				alert("Your password needs a minimum of six characters.");
 				return -1;
 			}
-			else if(password.length >30){
-				pass_veri += "Your password needs a maximum of thirty characters.";
+			else if(password.length > 30){
+				alert("Your password needs a maximum of thirty characters.");
 				return -1;
 			}
 			else if(password.search(/[a-z]/) < 0){
-				pass_veri += "Your password needs a lower case letter";
+				alert("Your password needs a lower case letter");
 				return -1;
 			}
 			else if(password.search(/[A-Z]/) < 0){
-				pass_veri += "Your password needs a upper case letter";
+				alert("Your password needs a upper case letter");
 				return -1;
 			}
 			else if (password.search(/[0-9]/) < 0) {
-   				pass_veri += "Your password needs a number";
+   				alert("Your password needs a number");
    				return -1;
 			}else{
 				return 1;
