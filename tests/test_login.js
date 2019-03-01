8c2d0f7996e4175237f7129479f07ac805971da5
Ziheng Shao <jjjjjames.szh@gmail.com>
2019-03-01 14:48:54 -0500

    unit test with json dataflow

---
+++ tests/test_login.js
@@ -0,0 +1,35 @@
+var request = require("supertest");
+var server = require("../app.js");
+var assert = require("assert");
+
+
+it('should return {"status":201}', function(done)
+{
+    request(server)
+        .post("/email_login")
+        .expect(200)
+        .expect(function(res)
+        {
+            assert.equal(res.text, '{"status":201}');
+        })
+        .end(done);
+});
+
+it('should return 200 with authentication token', function(done)
+{
+    let data = {
+        "email": "shao44@purdue.edu",
+        "password": "ShaoZH0923?"
+    }
+    request(server)
+        .post("/email_login")
+        .send(data)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(function(res)
+        {
+            assert.equal(res.body.status, 200);
+        })
+        .end(done);
+});
