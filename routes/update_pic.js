4e59eb75d05063f768d5f817ed8dadc0410ba53d
Ziheng Shao <jjjjjames.szh@gmail.com>
2019-03-01 13:20:36 -0500

    sample unit test for backend route

--- routes/update_pic.js
+++ routes/update_pic.js
@@ -2,33 +2,26 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 var fs = require('fs');
-var path = require('path');
 
 router.post('/', function(req, res, next){
     var auth_code = req.body.auth_token;
     var uid;
     var pic_id;
-    var pic_dir = './profile_pic/unknown.jpg';
+    var pic_dir = 'unknown.jpg';
 
     function base64_encode(file) {
-        //var bitmap = fs.readFileSync(file);
-        var bitmap = fs.readFileSync(path.join(__dirname, file));
+        var bitmap = fs.readFileSync(file);
         return new Buffer(bitmap).toString('base64');
     }
 
     function base64_decode(base64str, file) {
         var bitmap = new Buffer(base64str, 'base64');
-        fs.writeFileSync(path.join(__dirname, file), bitmap);
-        console.log('******** decoding complete ********');
+        fs.writeFileSync(file, bitmap);
+        console.log('******** 编码已转换为图片 ********');
     }
 
     var pic_stream = base64_encode(pic_dir);
-    //console.log(pic_stream);
-    //base64_decode(pic_stream, 'test.jpg');
-    res.json({
-        "status":200,
-        "pic":pic_stream
-    });
+    console.log(pic_stream);
 });
 
 module.exports = router;
