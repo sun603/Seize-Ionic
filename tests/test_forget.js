9cb5ee88a02487b945fcec50665c9b50a6b4e84f
WUu422 <wu1037@purdue.edu>
2019-03-05 01:11:12 -0500

    update test_forget.js

--- tests/test_forget.js
+++ tests/test_forget.js
@@ -21,10 +21,11 @@ it('should return {"status":201}', function(done)
         .end(done);
 });
 
-it('acquiring email from login', function(done)
+it('should return 200 by checking email from login', function(done)
 {
     let data = {
-        "email": "shao44@purdue.edu"
+        "email": "shao44@purdue.edu",
+        "password": "ShaoZH0923?"
     }
     request(server)
         .post("/email_login")
@@ -35,27 +36,7 @@ it('acquiring email from login', function(done)
         .expect(function(res)
         {
             assert.equal(res.body.status, 200);
-            code = res.body.code;
-            console.log("code = ", code);
         })
         .end(done);
 });
 
-it('checking if getting code correctly', function(done)
-{
-    let data = {
-        "code": code
-    }
-
-    request(server)
-        .post("/forget")
-        .send(data)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .expect(function(res)
-        {
-            assert.equal(res.body.status, 200);
-        })
-        .end(done);
-});
