2fcbe969ddba002e37139286c7147c82b259bac4
sun603 <sun603@users.noreply.github.com>
2019-03-18 15:13:07 -0400

    update api

--- src/app/settings/apisettings.ts
+++ src/app/settings/apisettings.ts
@@ -2,5 +2,12 @@ export const apisettings = {
   login:'/email_login',
   signup:'/signup',
   getavatar:'/get_pic',
-  getprofile:'getprofile',
+  getprofile:'/getprofile',
+  updateprofile:'/updateprofile',
+  updatepic:'update_pic',
+  logout:'/logout',
+  post:'post_seat',
+  search:'search_seat',
+  cencel:'cencel_seat',
+  // forget
 }
