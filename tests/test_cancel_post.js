864044a2dfd4eca465ba34717d340910f2c7252b
WUu422 <wu1037@purdue.edu>
2019-03-17 20:43:58 -0400

    test for cancel post 1.0.0

---
+++ tests/test_cancel_post.js
@@ -0,0 +1,50 @@
+var request = require("supertest");
+var server = require("../app.js");
+var assert = require("assert");
+
+var token;
+
+/*
+* testing getprofile for user:
+* email: shao44@purdue.edu
+* password: ShaoZH0923?
+* */
+it('acquiring token from login', function(done)
+{
+    let data = {
+        "email": "shao44@purdue.edu",
+        "password": "ShaoZH0923?"
+    }
+    request(server)
+        .post("/email_login")
+        .send(data)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(function(res)
+        {
+            assert.equal(res.body.status, 200);
+            auth_code = res.body.auth_token;
+            console.log("auth_code = ", auth_code);
+        })
+        .end(done);
+});
+
+it('checking if getting profiles correctly', function(done)
+{
+    let data = {
+        "auth_code": auth_code
+    }
+
+    request(server)
+        .post("/getprofile")
+        .send(data)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(function(res)
+        {
+            assert.equal(res.body.status, 200);
+        })
+        .end(done);
+});
