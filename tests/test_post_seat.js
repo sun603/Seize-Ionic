690495bb14031df96d79f191fbd9d123eb6dbab2
WUu422 <wu1037@purdue.edu>
2019-03-18 14:58:49 -0400

    update test post seat

--- tests/test_post_seat.js
+++ tests/test_post_seat.js
@@ -3,6 +3,8 @@ var server = require("../app.js");
 var assert = require("assert");
 
 var token;
+var seat_type;
+var noise_level;
 
 /*
 * testing getprofile for user:
@@ -25,19 +27,23 @@ it('acquiring token from login', function(done)
         {
             assert.equal(res.body.status, 200);
             token = res.body.auth;
-            console.log("token = ", token);
+            seat_type = res.body.seat_type;
+            noise_level = res.body.noise_level;
+            console.log("auth_token = ", token);
+            console.log("seat_type = ", seat_type);
+            console.log("noise_level = ", noise_level);
         })
         .end(done);
 });
 
-it('checking if getting profiles correctly', function(done)
+it('checking if posting seat correctly', function(done)
 {
     let data = {
         "auth_token": token
     }
 
     request(server)
-        .post("/getprofile")
+        .post("/post_seat")
         .send(data)
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
