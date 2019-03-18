6f388014634c76159c9a4d9376ba17d42372f4a4
WUu422 <wu1037@purdue.edu>
2019-03-17 20:43:42 -0400

    test for post seat 1.0.0

---
+++ tests/test_post_seat.js
@@ -0,0 +1,50 @@
+var request = require("supertest");
+var server = require("../app.js");
+var assert = require("assert");
+
+var token;
+
+/*
+* testing getprofile for user:
+* email: shao44@purdue.edu
+* password: ShaoZH0923?
+* */
+it('acquiring token from login', function(done)
+{
+    let data = {
+        "email": "shao44@purdue.edu",
+        "password": "ShaoZH0923?"
+    }
+    request(server)
+        .post("/email_login")
+        .send(data)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(function(res)
+        {
+            assert.equal(res.body.status, 200);
+            token = res.body.auth;
+            console.log("token = ", token);
+        })
+        .end(done);
+});
+
+it('checking if getting profiles correctly', function(done)
+{
+    let data = {
+        "auth_token": token
+    }
+
+    request(server)
+        .post("/getprofile")
+        .send(data)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(function(res)
+        {
+            assert.equal(res.body.status, 200);
+        })
+        .end(done);
+});
