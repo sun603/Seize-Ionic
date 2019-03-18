9beb4e0822286285369400eaec0d75231c60d560
Ziheng Shao <jjjjjames.szh@gmail.com>
2019-03-18 15:19:00 -0400

    test_post_seat

--- tests/test_post_seat.js
+++ tests/test_post_seat.js
@@ -1,10 +1,10 @@
 var request = require("supertest");
 var server = require("../app.js");
 var assert = require("assert");
+var mysql = require('mysql');
 
 var token;
-var seat_type;
-var noise_level;
+var uid;
 
 /*
 * testing getprofile for user:
@@ -27,19 +27,32 @@ it('acquiring token from login', function(done)
         {
             assert.equal(res.body.status, 200);
             token = res.body.auth;
-            seat_type = res.body.seat_type;
-            noise_level = res.body.noise_level;
             console.log("auth_token = ", token);
-            console.log("seat_type = ", seat_type);
-            console.log("noise_level = ", noise_level);
+
+            let uid_sql = "SELECT * FROM user_auth WHERE auth_code = \"" + token + "\"";
+            let uid_con = mysql.createConnection({
+                host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+                user: "shao44",
+                password: "ShaoZH0923?",
+                database: "cs307_sp19_team31"
+            });
+
+            uid_con.connect(function(err){
+                uid_con.query(uid_sql, function(err, result){
+                    uid = result[0].uid;
+                })
+            })
         })
         .end(done);
 });
 
-it('checking if posting seat correctly', function(done)
+it('post seat with loged in user', function(done)
 {
     let data = {
-        "auth_token": token
+        "auth_token": token,
+        "seat_type": "sofa",
+        "noise_level": 1,
+        "library":"hicks"
     }
 
     request(server)
@@ -53,4 +66,23 @@ it('checking if posting seat correctly', function(done)
             assert.equal(res.body.status, 200);
         })
         .end(done);
+});
+
+it('checking if info is in the posting pool', function(done){
+    let check_sql = "SELECT * FROM matching_pool WHERE uid = " + uid;
+    console.log("check_sql: ", check_sql);
+    let check_con = mysql.createConnection({
+        host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+        user: "shao44",
+        password: "ShaoZH0923?",
+        database: "cs307_sp19_team31"
+    });
+
+    check_con.connect(function(err){
+        check_con.query(check_sql, function(err, result){
+            assert.equal(result[0].seat_type, "sofa");
+            assert.equal(result[0].noise_level, 1);
+        });
+    })
+    done();
 });
