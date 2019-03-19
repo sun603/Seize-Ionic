f99981eeb73b30bfed2378b97cd768c9c51527b1
zhan2819 <jasmine199910@gmail.com>
2019-03-18 21:25:11 -0400

    find seat http

--- src/app/services/match.service.ts
+++ src/app/services/match.service.ts
@@ -39,4 +39,30 @@ export class MatchService {
   httppost(data){
     return this.http.post(environment.apiUrl+apisettings.post, data).pipe(map(res => res));
   }
+
+  find(data): Promise<any>{
+    return new Promise((resolve,reject) => {
+      this.httpfind(data).subscribe(
+        (val) =>{
+          if(val["status"]== 200){
+            resolve(val);
+          }else if(val["status"]== 201){
+            console.log("Logout at find for 201"+val);
+            this.auth.logout();
+            reject(val);
+          }else{
+            console.log("not sccuess in get http profile, but server on"+val["status"]);
+            reject(val);
+          }
+        },
+        err =>{
+          console.log("a connection err in get http find"+err);
+          reject(err);
+        }
+      );
+    });
+  }
+  httpfind(data){
+    return this.http.post(environment.apiUrl+apisettings.search, data).pipe(map(res => res));
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,56 @@
 import { Component } from '@angular/core';
+import { LibraryModel } from '../models/library.model'
+import { AuthenticationService } from '../services/authentication.service';
+import { MatchService } from '../services/match.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  library:any;
+  libs:any;
+  location:string;
+  seattype:string;
+  sound:number;
+  constructor(public auth: AuthenticationService, public matchService: MatchService,private router: Router,public alertController: AlertController){
+    this.library = new LibraryModel();
+  }
+  ngOnInit() { 
+    this.libs = this.library.getlibs();
+  }
+
+  find(){
+    this.auth.getauth().then(res => {
+      let data = {
+        "auth_token": res,
+        "library": location,
+        "seat_type": this.seattype,
+        "noise_level": this.sound,
+      };
+
+
+
+
+      this.matchService.find(data).then(data =>{
+        this.router.navigate(['/waiting']);
+      });
+      
+    })
+
+  }
+
+  async presentAlert(msg) {
+    const alert = await this.alertController.create({
+      // header: 'Alert',
+      subHeader: 'find',
+      message: msg,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+}
