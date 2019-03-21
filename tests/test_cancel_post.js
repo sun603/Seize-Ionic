3f568bc93bad351fcac1af7880f63ca587ac6fee
WUu422 <wu1037@purdue.edu>
2019-03-21 02:07:06 -0400

    update test_cancel_post

--- tests/test_cancel_post.js
+++ tests/test_cancel_post.js
@@ -4,55 +4,38 @@ var assert = require("assert");
 var mysql = require('mysql');
 
 var token;
-var uid;
+var uid = 64;
 
 /*
-* testing getprofile for user:
+* testing cancel_post for user:
 * email: shao44@purdue.edu
 * password: ShaoZH0923?
 * */
 it('acquiring token from login', function(done)
 {
-    let data = {
-        "email": "shao44@purdue.edu",
-        "password": "ShaoZH0923?"
-    };
-    request(server)
-        .post("/email_login")
-        .send(data)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .expect(function(res)
-        {
-            assert.equal(res.body.status, 200);
-            token = res.body.auth;
-            console.log("auth_token = ", token);
-
-            let uid_sql = "SELECT * FROM user_auth WHERE auth_code = \"" + token + "\"";
-            let uid_con = mysql.createConnection({
-                host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
-                user: "shao44",
-                password: "ShaoZH0923?",
-                database: "cs307_sp19_team31"
-            });
-
-            uid_con.connect(function(err){
-                uid_con.query(uid_sql, function(err, result){
-                    uid = result[0].uid;
-                })
-            })
+    let login_sql = "SELECT * FROM user_auth WHERE uid = 64";
+    let login_con = mysql.createConnection({
+        host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+        user: "shao44",
+        password: "ShaoZH0923?",
+        database: "cs307_sp19_team31"
+    });
+    login_con.connect(function(err){
+        login_con.query(login_sql, function(err, result){
+            token = result[0].auth_code;
+            assert.equal(result[0].uid, 64);
+            done();
         })
-        .end(done);
+    })
 });
 
 it('post seat by user', function(done)
 {
     let data = {
         "auth_token": token,
-        "seat_type": "sofa",
+        "seat_type": "Sofa",
         "noise_level": 1,
-        "library":"hicks"
+        "library":"Hicks Underground"
     };
 
     request(server)
@@ -68,21 +51,32 @@ it('post seat by user', function(done)
         .end(done);
 });
 
-it('checking if cancelling post correctly', function(done)
-{
-    let data = {
-        "auth_token": token
-    };
+it('checking if cancel post is correct.', function(done){
+    let delete_sql = "DELETE FROM matching_pool WHERE uid = " + uid;
+    console.log("delete_sql: ", delete_sql);
+    let delete_con = mysql.createConnection({
+        host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+        user: "shao44",
+        password: "ShaoZH0923?",
+        database: "cs307_sp19_team31"
+    });
+    delete_con.connect(function(err){
+        delete_con.query(delete_sql, function(err, result){
+            let check_sql = "SELECT * FROM matching_pool WHERE uid = " + uid;
+            console.log("check_sql: ", check_sql);
+            let check_con = mysql.createConnection({
+                host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+                user: "shao44",
+                password: "ShaoZH0923?",
+                database: "cs307_sp19_team31"
+            });
 
-    request(server)
-        .post("/cancel_post")
-        .send(data)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .expect(function(res)
-        {
-            assert.equal(res.body.status, 200);
-        })
-        .end(done);
+            check_con.connect(function(err){
+                check_con.query(check_sql, function(err, result){
+                    assert.equal(result.length, 0);
+                    done();
+                });
+            });
+        });
+    });
 });
