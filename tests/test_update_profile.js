be52fdc4624de97d23196b42dc720d8ad1dc7619
WUu422 <wu1037@purdue.edu>
2019-03-21 00:18:47 -0400

    promote test_update_profile

--- tests/test_update_profile.js
+++ tests/test_update_profile.js
@@ -4,7 +4,7 @@ var assert = require("assert");
 var mysql = require('mysql');
 
 var token;
-var uid;
+var uid = 64;
 
 /*
 * testing post_seat for user:
@@ -13,43 +13,26 @@ var uid;
 * */
 it('acquiring token from login', function(done)
 {
-    let data = {
-        "email": "test_user_1@purdue.edu",
-        "password": "TestUSER1"
-    };
-    request(server)
-        .post("/email_login")
-        .send(data)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .expect(function(res)
-        {
-            assert.equal(res.body.status, 200);
-            token = res.body.auth;
-            console.log("auth_token = ", token);
-
-            let uid_sql = "SELECT * FROM user_auth WHERE auth_code = \"" + token + "\"";
-            let uid_con = mysql.createConnection({
-                host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
-                user: "test_user1",
-                password: "TestUSER1",
-                database: "cs307_sp19_team31"
-            });
-
-            uid_con.connect(function(err){
-                uid_con.query(uid_sql, function(err, result){
-                    uid = result[0].uid;
-                })
-            })
+    let login_sql = "SELECT * FROM user_auth WHERE uid = 64";
+    let login_con = mysql.createConnection({
+        host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+        user: "shao44",
+        password: "ShaoZH0923?",
+        database: "cs307_sp19_team31"
+    });
+    login_con.connect(function(err){
+        login_con.query(login_sql, function(err, result){
+            token = result[0].auth_code;
+            assert.equal(result[0].uid, 64);
+            done();
         })
-        .end(done);
+    })
 });
-
 
 it ('update profile by user', function(done)
 {
     let data = {
+        "auth_token": token,
         "name": "test_user_1",
         "gender": "Male",
         "school": "Purdue University (West Lafayette)",
@@ -70,7 +53,7 @@ it ('update profile by user', function(done)
          .end(done);
 });
 
-it('checking if info is updated to profile', function(done){
+it('checking if name is updated to profile', function(done){
      let check_sql = "SELECT * FROM profile WHERE uid = " + uid;
      console.log("check_sql: ", check_sql);
      let check_con = mysql.createConnection({
@@ -82,13 +65,98 @@ it('checking if info is updated to profile', function(done){
 
      check_con.connect(function(err){
          check_con.query(check_sql, function(err, result){
-             assert.equal(result[0].name, "Ziheng Shao");
-             assert.equal(result[0].gender, "Male");
-             assert.equal(result[0].school, "Purdue University (West Lafayette)");
-             assert.equal(result[0].class, "Junior");
-             assert.equal(result[0].major, "Computer Science");
-             assert.equal(result[0].description, null);
+             assert.equal(result[0].name, "test_user_1");
          });
      });
      done();
  });
+
+it('checking if gender is updated to profile', function(done){
+    let check_sql = "SELECT * FROM profile WHERE uid = " + uid;
+    console.log("check_sql: ", check_sql);
+    let check_con = mysql.createConnection({
+        host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+        user: "shao44",
+        password: "ShaoZH0923?",
+        database: "cs307_sp19_team31"
+    });
+
+    check_con.connect(function(err){
+        check_con.query(check_sql, function(err, result){
+            assert.equal(result[0].gender, "Male");
+        });
+    });
+    done();
+});
+
+it('checking if school is updated to profile', function(done){
+    let check_sql = "SELECT * FROM profile WHERE uid = " + uid;
+    console.log("check_sql: ", check_sql);
+    let check_con = mysql.createConnection({
+        host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+        user: "shao44",
+        password: "ShaoZH0923?",
+        database: "cs307_sp19_team31"
+    });
+
+    check_con.connect(function(err){
+        check_con.query(check_sql, function(err, result){
+            assert.equal(result[0].school, "Purdue University (West Lafayette)");
+        });
+    });
+    done();
+});
+
+it('checking if class is updated to profile', function(done){
+    let check_sql = "SELECT * FROM profile WHERE uid = " + uid;
+    console.log("check_sql: ", check_sql);
+    let check_con = mysql.createConnection({
+        host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+        user: "shao44",
+        password: "ShaoZH0923?",
+        database: "cs307_sp19_team31"
+    });
+
+    check_con.connect(function(err){
+        check_con.query(check_sql, function(err, result){
+            assert.equal(result[0].class, "Junior");
+        });
+    });
+    done();
+});
+
+it('checking if major is updated to profile', function(done){
+    let check_sql = "SELECT * FROM profile WHERE uid = " + uid;
+    console.log("check_sql: ", check_sql);
+    let check_con = mysql.createConnection({
+        host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+        user: "shao44",
+        password: "ShaoZH0923?",
+        database: "cs307_sp19_team31"
+    });
+
+    check_con.connect(function(err){
+        check_con.query(check_sql, function(err, result){
+            assert.equal(result[0].major, "Computer Science");
+        });
+    });
+    done();
+});
+
+it('checking if description is updated to profile', function(done){
+    let check_sql = "SELECT * FROM profile WHERE uid = " + uid;
+    console.log("check_sql: ", check_sql);
+    let check_con = mysql.createConnection({
+        host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+        user: "shao44",
+        password: "ShaoZH0923?",
+        database: "cs307_sp19_team31"
+    });
+
+    check_con.connect(function(err){
+        check_con.query(check_sql, function(err, result){
+            assert.equal(result[0].description, null);
+        });
+    });
+    done();
+});
