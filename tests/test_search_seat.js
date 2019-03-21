d2f9cec5f5781d6bb173fa56e3641647f7e2914e
Ziheng Shao <jjjjjames.szh@gmail.com>
2019-03-21 02:26:18 -0400

    Update test_search_seat.js

--- tests/test_search_seat.js
+++ tests/test_search_seat.js
@@ -69,9 +69,12 @@ it('post seat by user', function(done)
         .end(done);
 });
 
-it ('check for correct auth_token', function(done){
+it ('check for correct auth_token and search', function(done){
     let data = {
-        "auth_token": token
+        "auth_token": token,
+        "seat_type": "Sofa",
+        "noise_level": 1,
+        "library":"Hicks Undergraduate"
     };
     request(server)
         .post("/search_seat")
@@ -82,6 +85,24 @@ it ('check for correct auth_token', function(done){
         .expect(function(res)
         {
             assert.equal(res.body.status, 200);
+            assert.equal(res.body.uid, 64);
         })
         .end(done);
 })
+
+it ('delete the testing post', function(done){
+    let delete_sql = "delete from matching_pool where uid < 65";
+    let delete_con = mysql.createConnection({
+        host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+        user: "shao44",
+        password: "ShaoZH0923?",
+        database: "cs307_sp19_team31"
+    });
+
+    delete_con.connect(function(err){
+        delete_con.query(delete_sql, function(err, result){
+            assert.equal(0, 0);
+            done();
+        });
+    });
+});
