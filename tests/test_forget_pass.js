c81fd2d3edda6493c722cd9bf518ad27c99d2464
WUu422 <wu1037@purdue.edu>
2019-04-07 19:10:49 -0400

    update forget_pass

--- tests/test_forget_pass.js
+++ tests/test_forget_pass.js
@@ -18,6 +18,24 @@ it('should return {"status":201}', function(done)
         })
         .end(done);
 });
+
+it('acquiring token from login', function(done)
+{
+    let login_sql = "SELECT * FROM user_auth WHERE uid = 64";
+    let login_con = mysql.createConnection({
+        host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+        user: "shao44",
+        password: "ShaoZH0923?",
+        database: "cs307_sp19_team31"
+    });
+    login_con.connect(function(err){
+        login_con.query(login_sql, function(err, result){
+            token = result[0].auth_code;
+            assert.equal(result[0].uid, 64);
+            done();
+        })
+    })
+});
 /*
 it('should return 200 by checking new password', function(done)
 {
