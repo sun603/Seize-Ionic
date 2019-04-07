81b9dbd41487350602a29a045b5b6e5a679b8493
WUu422 <wu1037@purdue.edu>
2019-04-07 17:55:34 -0400

    update test_logout.js

--- tests/test_logout.js
+++ tests/test_logout.js
@@ -1,24 +1,54 @@
 var request = require("supertest");
 var server = require("../app.js");
 var assert = require("assert");
+var mysql = require('mysql');
 
-it('should return 200 with authentication token', function(done)
+var token;
+var uid = 64;
+
+/*
+* testing logout for user:
+* email: shao44@purdue.edu
+* password: ShaoZH0923?
+* */
+it('acquiring token from login', function(done)
 {
-    let data = {
-        "email": "shao44@purdue.edu",
-        "password": "ShaoZH0923?"
-    };
-    request(server)
-        .post("/email_login")
-        .send(data)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .expect(function(res)
-        {
-            assert.equal(res.body.status, 200);
-            //token = res.body.auth;
+    let login_sql = "SELECT * FROM user_auth WHERE uid = 64";
+    let login_con = mysql.createConnection({
+        host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+        user: "shao44",
+        password: "ShaoZH0923?",
+        database: "cs307_sp19_team31"
+    });
+    login_con.connect(function(err){
+        login_con.query(login_sql, function(err, result){
+            token = result[0].auth_code;
+            assert.equal(result[0].uid, 64);
+            done();
         })
-        .end(done);
+    })
 });
+
+it('checking if cancel post is correct.', function(done){
+    let delete_sql_1 = "DELETE FROM user_auth WHERE uid = " + uid;
+    let delete_sql_2 = "DELETE FROM matching_pool WHERE uid = " + uid;
+
+    let delete_con = mysql.createConnection({
+        host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+        user: "shao44",
+        password: "ShaoZH0923?",
+        database: "cs307_sp19_team31"
+    });
+    delete_con.connect(function(err){
+        delete_con.query(delete_sql, function(err, result){
+            delete_con.query(delete_sql_1, function(err, result){});
+            delete_con.query(delete_sql_2, function(err, result){});
+            delete_con.destroy();
+            res.json({
+                "status": 200
+            });
+        });
+    });
+});
+
 
