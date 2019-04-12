f5fb3a7251c2f70fc90d5be2ff5f33a71058f6ab
Ziheng Shao <jjjjjames.szh@gmail.com>
2019-04-12 19:34:25 -0400

    Update get_friend_profile.js

--- routes/get_friend_profile.js
+++ routes/get_friend_profile.js
@@ -17,98 +17,117 @@ router.post('/', function(req, res, next){
     let uid_string = req.body.uid + ""; // array, 长字符串
     let uid_array = uid_string.split(",");
 
-    let result = [];
+    let final_result = [];
 
-    uid_array.forEach(function(uid) {
-        var search_sql = "SELECT * FROM profile WHERE uid = " + uid;
-        var search_con = mysql.createConnection({
-            host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
-            user: "shao44",
-            password: "ShaoZH0923?",
-            database: "cs307_sp19_team31"
-        });
+    function f1() {
+        return new Promise(function (resolve) {
+            uid_array.forEach(function (uid) {
+                var search_sql = "SELECT * FROM profile WHERE uid = " + uid;
+                var search_con = mysql.createConnection({
+                    host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+                    user: "shao44",
+                    password: "ShaoZH0923?",
+                    database: "cs307_sp19_team31"
+                });
 
-        search_con.connect(function (err) {
-            if (err) {
-                search_con.destroy();
-                res.json({
-                    "status": 404,
-                    "error message": "database connection error"
-                })
-            } else {
-                search_con.query(search_sql, function (err, result) {
-                    if (result === undefined) {
+                search_con.connect(function (err) {
+                    if (err) {
                         search_con.destroy();
                         res.json({
-                            "status": 201,
-                            "error message": "user does not exist"
-                        })
-                    } else if (result.length === 0) {
-                        search_con.destroy();
-                        res.json({
-                            "status": 201,
-                            "error message": "user does not exist"
+                            "status": 404,
+                            "error message": "database connection error"
                         })
                     } else {
-                        let name = result[0].name;
-                        let pid = result[0].pic_id;
-                        search_con.destroy();
+                        search_con.query(search_sql, function (err, result) {
+                            if (result === undefined) {
+                                search_con.destroy();
+                                res.json({
+                                    "status": 201,
+                                    "error message": "user does not exist"
+                                })
+                            } else if (result.length === 0) {
+                                search_con.destroy();
+                                res.json({
+                                    "status": 201,
+                                    "error message": "user does not exist"
+                                })
+                            } else {
+                                let name = result[0].name;
+                                let pid = result[0].pic_id;
+                                search_con.destroy();
 
-                        if (pid === null) {
-                            let pic_dir = './profile_pic/' + "unknown.jpg";
-                            let pic_stream = base64_encode(pic_dir);
-                            res.json({
-                                "status": 200,
-                                "name": name,
-                                "pic": pic_stream
-                            });
-                        } else {
-
-                            let pic_sql = "SELECT * FROM profile_pic WHERE pic_id = " + pid;
-                            let pic_con = mysql.createConnection({
-                                host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
-                                user: "shao44",
-                                password: "ShaoZH0923?",
-                                database: "cs307_sp19_team31"
-                            });
-
-                            pic_con.connect(function (err) {
-                                if (err) {
-                                    pic_con.destroy();
+                                if (pid === null) {
+                                    let pic_dir = './profile_pic/' + "unknown.jpg";
+                                    let pic_stream = base64_encode(pic_dir);
                                     res.json({
-                                        "status": 404,
-                                        "error message": "database connection error"
+                                        "status": 200,
+                                        "name": name,
+                                        "pic": pic_stream
                                     });
                                 } else {
 
-                                    pic_con.query(pic_sql, function (err, result) {
-                                        let pic_dir = './profile_pic/' + result[0].pic_file_name;
-                                        let pic_stream = base64_encode(pic_dir);
-                                        pic_con.destroy();
-                                        // res.json({
-                                        //     "status": 200,
-                                        //     "name": name,
-                                        //     "pic": pic_stream
-                                        // });
-                                        let temp_data = {
-                                            "uid": uid,
-                                            "name": name,
-                                            "pic": pic_stream
-                                        };
-                                        result.push(temp_data);
-                                        console.log(result);
+                                    let pic_sql = "SELECT * FROM profile_pic WHERE pic_id = " + pid;
+                                    let pic_con = mysql.createConnection({
+                                        host: "cs307-spring19-team31.c2n62lnzxryr.us-east-2.rds.amazonaws.com",
+                                        user: "shao44",
+                                        password: "ShaoZH0923?",
+                                        database: "cs307_sp19_team31"
+                                    });
+
+                                    pic_con.connect(function (err) {
+                                        if (err) {
+                                            pic_con.destroy();
+                                            res.json({
+                                                "status": 404,
+                                                "error message": "database connection error"
+                                            });
+                                        } else {
+
+                                            pic_con.query(pic_sql, function (err, result) {
+                                                let pic_dir = './profile_pic/' + result[0].pic_file_name;
+                                                let pic_stream = base64_encode(pic_dir);
+                                                pic_con.destroy();
+                                                // res.json({
+                                                //     "status": 200,
+                                                //     "name": name,
+                                                //     "pic": pic_stream
+                                                // });
+                                                let temp_data = {
+                                                    "index": uid,
+                                                    "name": name,
+                                                    "pic": pic_stream
+                                                };
+                                                final_result.push(temp_data);
+                                                console.log("result pushed");
+                                            })
+                                        }
                                     })
                                 }
-                            })
-                        }
 
+                            }
+                        });
                     }
                 });
-            }
+            });
+            setTimeout(resolve, 1000);
         });
-    });
+    }
 
-    console.log("final result: ", result);
+    async function f2() {
+        // console.log('before callback');
+        // callback();
+        // console.log('after callback');
+        await f1();
+        console.log("final result: ", final_result);
+        let return_data = {
+            "status": 200,
+            "info": final_result
+        }
+        res.json(return_data);
+    }
+
+    f2();
+
 
 
     function base64_encode(file) {
