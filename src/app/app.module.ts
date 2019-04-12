22ca8bf75103069ce76e495beb1014a9928f7ab1
sun603 <sun603@users.noreply.github.com>
2019-04-12 18:43:07 -0400

    add componets: feed message friendlist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,9 +19,13 @@ import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { FeedComponent } from './components/feed/feed.component';
+import { MessageComponent } from './components/message/message.component';
+import { FriendlistComponent } from './components/friendlist/friendlist.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FeedComponent, MessageComponent, FriendlistComponent],
   entryComponents: [],
   imports: [
     BrowserModule, 
@@ -30,9 +34,10 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
     IonicStorageModule.forRoot(),
     HttpClientModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
-    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
-    AngularFireStorageModule // imports firebase/storage only needed for storage features
+    // AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    // AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    // AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    AngularFireDatabaseModule,
   ],
   providers: [
     StatusBar,
---
+++ src/app/components/friendlist/friendlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-friendlist',
+  templateUrl: './friendlist.component.html',
+  styleUrls: ['./friendlist.component.scss']
+})
+export class FriendlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
