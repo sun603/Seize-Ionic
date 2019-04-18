5478e59c66a78a48e4a6b3390e219fa70570fb4f
Ziheng Shao <jjjjjames.szh@gmail.com>
2019-04-17 23:22:30 -0400

    update ui in chat page

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
-import { FeedComponent } from './feed/feed.component';
+import { FeedComponent } from '../feed/feed.component';
 import { MessageComponent } from './message/message.component';
 import { FriendlistComponent } from './friendlist/friendlist.component';
 import { PopoverComponent } from './popover/popover.component';
