51b7ea7322eb383d67ebcac1e9f8c45fc48765e2
Ziheng Shao <jjjjjames.szh@gmail.com>
2019-04-17 20:49:29 -0400

    push first greeting message for recently matched users

--- routes/add_friends.js
+++ routes/add_friends.js
@@ -8,6 +8,16 @@ var config = {
 };
 
 function add_friend(uid, friend_uid){
+    const now = new Date();
+    const date = now.getUTCFullYear() + '/' +
+        (now.getUTCMonth() + 1) + '/' +
+        now.getUTCDate();
+    const time = now.getUTCHours() + ':' +
+        now.getUTCMinutes() + ':' +
+        now.getUTCSeconds();
+
+    let real_time = date + ' ' + time;
+
     firebase.initializeApp(config);
     let database = firebase.database();
     let data = {
@@ -19,6 +29,28 @@ function add_friend(uid, friend_uid){
         "id": uid
     };
     database.ref('friend_list/' + friend_uid + '/' + uid).update(data);
+
+    var room_id;
+    if (uid < friend_uid){
+        room_id = uid + '' + friend_uid;
+    }
+    else{
+        room_id = friend_uid + '' + uid;
+    }
+
+    data = {
+        "id": uid,
+        "message": "hey what's up!",
+        "timestamp": real_time
+    };
+    database.ref('room-message/' + room_id).push(data);
+
+    data = {
+        "id": friend_uid,
+        "message": "hey what's up!",
+        "timestamp": real_time
+    };
+    database.ref('room-message/' + room_id).push(data);
 
 }
 
